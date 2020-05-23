bc198f810414d7a841e8f893d6bad0e31f09fb7d
Tom Duncalf <tom@tomduncalf.com>
2020-05-23 16:32:56 +0100

    Fix example

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -2,18 +2,27 @@ import { useState, useEffect } from "react";
 
 class CountStore {
   count = 0;
+  setupComplete = false;
 }
 
 const store = new CountStore();
 
 export default () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(store.count);
 
   useEffect(() => {
-    setInterval(() => {
+    if (store.setupComplete) return;
+
+    const interval = setInterval(() => {
       store.count++;
       setCount(store.count);
     }, 1000);
+    store.setupComplete = true;
+
+    return () => {
+      store.setupComplete = false;
+      clearInterval(interval);
+    };
   }, []);
 
   return <div>Count from store: {count}</div>;
